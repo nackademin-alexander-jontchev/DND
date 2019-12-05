aadac873b2610fd42f99c0c34e8289247ae8ec8b
Saidsnackademin <57669167+Saidsnackademin@users.noreply.github.com>
2019-12-05 12:29:27 +0100

    file for the hero Knight

---
+++ heros.py
@@ -0,0 +1,9 @@
+class knight:
+    def __init__(self, initiativ, durability, attack, agility):
+        self.initiativ = 5
+        self.durability = 9
+        self.attack = 6
+        self.agility = 5
+        
+    def ability_discription():
+        #first attack is blocket by defoult, during the battle phase the Knight dosent need to block or evade 
