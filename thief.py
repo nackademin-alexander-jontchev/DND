c93096e47d8d51aa25d21468e4646973e7145da0
este001 <estefan.rm2@gmail.com>
2019-12-05 13:33:17 +0100

    added thief and wizard

---
+++ thief.py
@@ -0,0 +1,15 @@
+class thief():
+
+    def __init__(self):
+        self.initiative = 6
+        self.durability = 4
+        self.attack = 9
+        self.agility = 5
+
+    def ability_discription(self):
+        print("You have chosen the noble knight")
+        print(f"Initiativ =  {self.initiativ}")
+        print(f"Durability = {self.durability}")
+        print(f"Attack = {self.attack}")
+        print(f"Agility = {self.agility}")
+        print(f"The Critical strike\n the thief has a 25% to do double damage everytime the thief attacks")
---
+++ wizard.py
@@ -0,0 +1,15 @@
+class wizard:
+
+    def __init__(self):
+        self.initiative = 6
+        self.durability = 4
+        self.attack = 9
+        self.agility = 5
+
+    def ability_discription(self):
+        print("You have chosen the noble knight")
+        print(f"Initiativ =  {self.initiativ}")
+        print(f"Durability = {self.durability}")
+        print(f"Attack = {self.attack}")
+        print(f"Agility = {self.agility}")
+        print(f"The Wizards speciall ability is Light flash:\n The wizard can blind the monsters and has an 80% chance to escape fights")
