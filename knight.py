bec9363598ac526b97b6c2103e9a1fa6918533cc
este001 <estefan.rm2@gmail.com>
2019-12-05 13:28:53 +0100

    resolved error

--- knight.py
+++ knight.py
@@ -1,10 +1,9 @@
 class knight:
-    def __init__(self, initiativ, durability, attack, agility):
+    def __init__(self):
         self.initiativ = 5
         self.durability = 9
         self.attack = 6
         self.agility = 5
-<<<<<<< HEAD:heros.py
         
     def ability_discription(self):
         print("You have chosen the noble knight")
@@ -12,9 +11,4 @@ class knight:
         print(f"Durability = {self.durability}")
         print(f"Attack = {self.attack}")
         print(f"Agility = {self.agility}")
-        print(f("The knigts speciall ability is shieldblock:\n att the start of a battle, the knight has a 100% chans of blocking the first attack"))
-=======
-
-    def ability_discription(self):
-        print("First attack is blocket by defoult, during the battle phase the Knight dosent need to block or evade")
->>>>>>> a83057750b6b158850bde8604c56db641aa22a84:knight.py
+        print(f"The knigts speciall ability is shieldblock:\n att the start of a battle, the knight has a 100% chans of blocking the first attack")
