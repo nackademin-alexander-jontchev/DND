5ee3796bc9e06768762892c05c7c63ae84c730e8
Captain-ops <57669616+Captain-ops@users.noreply.github.com>
2019-12-09 13:53:07 +0100

    Update knight.py

--- knight.py
+++ knight.py
@@ -6,16 +6,8 @@ class Knight:
         self.attack = 6
         self.agility = 5
         
-    def ability_discription(self):
-        print("\n1-Knight\n")
-
-        print(f"Initiative =  {self.initiative}")
-        print(f"Durability = {self.durability}")
-        print(f"Attack = {self.attack}")
-        print(f"Agility = {self.agility}")
-        print(f"The knight special ability is shieldblock:\nAt the start of a battle, the knight has a 100% chance of blocking the first attack")
-
-
+    def ability_discription(self)
+    
         print("\n1-Knight")
         print(f" ---------------")
         print(f"| Initiative = {self.initiative}|")
