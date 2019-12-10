9e56610c2ce845b1ecfd65bad35f508c9d4bc9e2
Captain-ops <Yassine.Imlil@yh.nackademin.se>
2019-12-10 11:15:55 +0100

    removed monsters

--- giant_spider.py
+++
@@ -1,9 +0,0 @@
-class giant_spider:
-    
-    def __init__(self):
-        self.initiative = 7
-        self.durability = 1
-        self.attack = 2
-        self.agility = 3
-        self.spawn = 0.20
-        #spawn chance is 20%
